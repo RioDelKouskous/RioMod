525727c373a01af9aa344ce6ae0a4ad99e20638f
Suiveurtag <suiveurtag44@gmail.com>
2026-05-24 09:53:07 +0200

    remove references

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: BalaRio
 --- MOD_ID: BalaRio
---- MOD_AUTHOR: [RioSkai]
+--- MOD_AUTHOR: [RioSkai, Suiveurtag]
 --- MOD_DESCRIPTION: Cool mod yeah yeah.
 --- PREFIX: xmpl
 ----------------------------------------------
@@ -22,50 +22,3 @@ if err then
 else
     f()
 end
-
-----------------------------------------------
------------- TAROT DEFINITIONS ---------------
-
-SMODS.Tarot{
-    key = 'rio_guy',
-    atlas = 'rio_assets',
-    pos = {x = 0, y = 0},
-    cost = 3,
-    unlocked = true,
-    discovered = true,
-    calculate = function(self, card, context)
-        if context.using_consumable then
-            for i = 1, 2 do
-                local new_card = create_card('Tarot', G.consumeables, nil, nil, nil, nil, nil, 'rio')
-                new_card:add_to_deck()
-                G.consumeables:emplace(new_card)
-            end
-            return {
-                message = localize('k_xmpl_live'),
-                colour = G.C.PURPLE
-            }
-        end
-    end
-}
-
-----------------------------------------------
------------ BOOSTER DEFINITIONS --------------
-
-SMODS.Booster{
-    key = 'rio_pack',
-    kind = 'Tarot',
-    atlas = 'rio_assets',
-    pos = {x = 1, y = 0},
-    config = {extra = 3, choose = 1}, -- Look at 3, choose 1
-    cost = 4,
-    weight = 2, -- How often it appears
-    unlocked = true,
-    discovered = true,
-    loc_vars = function(self, info_queue, center)
-        return {vars = {center.config.choose, center.config.extra}}
-    end,
-    create_card = function(self, card, i)
-        return create_card('Tarot', G.pack_cards, nil, nil, nil, nil, nil, 'rio_p')
-    end,
-    group_key = 'k_tarot_pack'
-}
